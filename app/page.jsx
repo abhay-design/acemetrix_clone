b86e5ed58b68d29f62aa53689655876255783422
Abhay <abhay@cleardigital.com>
2025-03-25 17:42:04 +0530

    update banner

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import { Poppins } from "next/font/google";
+import Banner from "../components/banner";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700"],
@@ -7,5 +8,9 @@ const poppins = Poppins({
 });
 
 export default function Home() {
-  return <main className={poppins.className}></main>;
+  return (
+    <main className={poppins.className}>
+      <Banner />
+    </main>
+  );
 }
---
+++ components/banner.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+const Banner = () => {
+  return (
+    <section className="banner min-h-[964px] flex items-center">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="banner-video object-cover"
+        width={1000}
+        height={1000}
+      >
+        <source src="/videos/home-kayak.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="container">
+        <div className="wrapper text-center">
+          <h1 className="text-white">
+            Ace Metrix Measures the Impact of Video Advertising
+          </h1>
+          <h2 className="text-white">
+            Technology, tools and insights to design creative to drive business
+            results
+          </h2>
+          <div className="btn-wrap relative">
+            <Link href="#" className="btn-clear">
+              Watch Video
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Banner;
